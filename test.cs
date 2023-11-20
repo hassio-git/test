4aaabcb828be283afc16a769bff822c713b42589
hassio-git <61124205+hassio-git@users.noreply.github.com>
2023-11-20 15:07:50 +0100

    Update test.cs

--- test.cs
+++ test.cs
@@ -28,14 +28,5 @@ class Program
         Console.WriteLine("Hello, World!");
 
 		File.Copy ("c:\\windows\\system32\\cmd.exe", "H:\\Kairos\\Temp\\OperatorConsole_Cache\\Extensions\\NextVideoServerCCTV\\lol.exe");
-//		File.Copy ("c:\\windows\\system32\\cmd.exe", "c:\\program files\\telewizja\\cmd.exe");
-		Console.ReadLine();
-//        Process p = new Process();
-//        p.StartInfo.FileName = "H:\\Kairos\\Temp\\OperatorConsole_Cache\\Extensions\\NextVideoServerCCTV\\test.exe";
-//        p.StartInfo.WorkingDirectory = @"H:\\Kairos\\Temp\\OperatorConsole_Cache\\Extensions\\NextVideoServerCCTV\\";
-//        p.StartInfo.Arguments = "-i cmd.exe";
-//        p.Start();
-		Console.ReadLine();
-//		File.Copy ("c:\\windows\\system32\\cmd.exe", "H:\\Kairos\\Temp\\OperatorConsole_Cache\\Extensions\\NextVideoServerCCTV\\cmd.exe");
 	}
 }
